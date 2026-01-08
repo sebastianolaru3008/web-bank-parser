b015cfb328f87d95651b391c9732c9eeaccd2035
Sebastian Olaru <sebastianolaru3008@gmail.com>
2026-01-08 17:33:34 +0200

    fix: Update allowedHosts in Vite configuration for production environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://backend:8080',
-        changeOrigin: true,
-        secure: false
-      }
-    },
-    allowedHosts:[
-        "a3b3d848e87a.ngrok-free.app"
-    ]
-  }
+    plugins: [sveltekit()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://backend:8080',
+                changeOrigin: true,
+                secure: false
+            }
+        },
+        allowedHosts: [
+            "expenses-webparser-smmhnj-1dc82d-31-97-115-27.traefik.me"
+        ]
+    }
 };
 
 export default config;
